34f19a05b75012e28a05ede79a64545a06ec8d78
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-07-04 16:06:46 -0400

    Add files via upload

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,9 +1,14 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import Head from "next/head";
 import { motion } from "framer-motion-3d";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import * as THREE from "three";
-import { OrbitControls, MeshReflectorMaterial } from "@react-three/drei";
+import {
+  MeshReflectorMaterial,
+  FirstPersonControls,
+  OrbitControls,
+  Float
+} from "@react-three/drei";
 
 const store = () => {
   return (
@@ -13,11 +18,15 @@ const store = () => {
       </Head>
 
       <main className='min-h-screen'>
-        <Canvas>
+        <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
+          {/* <color attach='background' args={["#010203"]} /> */}
+          <fog attach='fog' args={["#010203", 0, 15]} />
           <OrbitControls />
-          <ambientLight />
-          <directionalLight color='blue' position={[10, 10, 15]} />
-          <Torus position={[1, 1, 0]} />
+          <ambientLight args={["#0D0D0D", 1.2]} />
+          <pointLight intensity={2} color='#fc0000' position={[0, 5, 5]} />
+          <Float>
+            <Torus position={[0, 1, -5]} />
+          </Float>
           <Plane />
         </Canvas>
       </main>
@@ -25,21 +34,39 @@ const store = () => {
   );
 };
 
+const FPControls = () => {
+  const args = {
+    activeLook: true,
+    autoForward: false,
+    constrainVertical: false,
+    enabled: true,
+    heightCoef: 1,
+    heightMax: 1,
+    heightMin: 0,
+    heightSpeed: false,
+    lookVertical: true,
+    lookSpeed: 0.005,
+    movementSpeed: 3,
+    verticalMax: Math.PI,
+    verticalMin: 0
+  };
+  return <FirstPersonControls {...args} />;
+};
+
 const Plane = () => {
   return (
-    <motion.mesh position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <motion.planeBufferGeometry args={[50, 50]} color='blue' />
+    <motion.mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <motion.planeBufferGeometry args={[50, 50]} />
       <MeshReflectorMaterial
-        mirror={0}
-        blur={[300, 100]}
+        mirror={1}
+        blur={[400, 100]}
         resolution={2048}
         mixBlur={1}
-        mixStrength={40}
-        roughness={1}
-        depthScale={1.2}
+        mixStrength={30}
+        depthScale={15}
         minDepthThreshold={0.4}
-        maxDepthThreshold={1.4}
-        color='#101010'
+        color='#0D0D0D'
+        roughness={1}
         metalness={0.5}
       />
     </motion.mesh>
@@ -48,11 +75,19 @@ const Plane = () => {
 
 const Torus = (props: any) => {
   const ref = useRef<THREE.Mesh>(null);
-  useFrame(() => (ref.current.rotation.x += 0.01));
+  const [hover, setHover] = useState(false);
   return (
-    <motion.mesh ref={ref} {...props}>
+    <motion.mesh
+      ref={ref}
+      {...props}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
       <torusBufferGeometry attach='geometry' />
-      <meshStandardMaterial attach='material' color='orange' />
+      <meshStandardMaterial
+        attach='material'
+        color={hover ? "#AC7979" : "white"}
+      />
     </motion.mesh>
   );
 };
