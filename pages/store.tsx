0d1995f2a5a225039a88306c9910bd9e4ce4b8c5
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-07-05 12:45:53 -0400

    Add files via upload

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,14 +1,16 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import Head from "next/head";
 import { motion } from "framer-motion-3d";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import {
   MeshReflectorMaterial,
   FirstPersonControls,
   OrbitControls,
-  Float
+  Float,
+  useTexture
 } from "@react-three/drei";
+import { LinearEncoding, type Texture } from "three";
 
 const store = () => {
   return (
@@ -18,14 +20,14 @@ const store = () => {
       </Head>
 
       <main className='min-h-screen'>
-        <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
+        <Canvas camera={{ position: [0, 1, 5], fov: 50 }} shadows>
           {/* <color attach='background' args={["#010203"]} /> */}
           <fog attach='fog' args={["#010203", 0, 15]} />
           <OrbitControls />
           <ambientLight args={["#0D0D0D", 1.2]} />
           <pointLight intensity={2} color='#fc0000' position={[0, 5, 5]} />
           <Float>
-            <Torus position={[0, 1, -5]} />
+            <Torus castShadow receiveShadow position={[0, 1, -5]} />
           </Float>
           <Plane />
         </Canvas>
@@ -54,20 +56,44 @@ const FPControls = () => {
 };
 
 const Plane = () => {
+  const textures = useTexture({
+    map: "./textures/concrete/diffuse.jpg",
+    displacementMap: "./textures/concrete/displacement.jpg",
+    aoMap: "./textures/concrete/arm.jpg",
+    roughnessMap: "./textures/concrete/arm.jpg",
+    metalnessMap: "./textures/concrete/arm.jpg"
+  });
+
+  const normalTexture = useTexture(
+    "./textures/concrete/normal.jpg",
+    (texture) => {
+      (texture as Texture).encoding = LinearEncoding;
+    }
+  );
   return (
-    <motion.mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+    <motion.mesh
+      position={[0, 0, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      castShadow
+      receiveShadow
+    >
       <motion.planeBufferGeometry args={[50, 50]} />
       <MeshReflectorMaterial
-        mirror={1}
+        {...textures}
+        normalMap={normalTexture}
+        dithering={true}
+        mirror={0}
         blur={[400, 100]}
-        resolution={2048}
-        mixBlur={1}
-        mixStrength={30}
-        depthScale={15}
-        minDepthThreshold={0.4}
+        resolution={1024}
+        mixBlur={30}
+        mixStrength={80}
+        mixContrast={1}
+        depthScale={1}
+        minDepthThreshold={0.1}
         color='#0D0D0D'
         roughness={1}
         metalness={0.5}
+        reflectorOffset={0.2}
       />
     </motion.mesh>
   );
